42bb88fb062a68a9e030e4025310fbd4de4bb989
samiwokx <samsaravillo@gmail.com>
2021-02-09 21:39:54 +0800

    added progress component

---
+++ src/components/progress.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Progress = () => {
+  return <div>Sample Progress</div>;
+};
+
+export default Progress;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Progress from './components/progress';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Progress />
   </React.StrictMode>,
   document.getElementById('root')
 );
